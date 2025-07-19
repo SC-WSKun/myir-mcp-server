c582895ac8b608eccfea098579ddf2568988c29a
kunkun <540610423@qq.com>
2025-07-19 16:54:39 +0800

    feat: add move to point & move back tool

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import requests
 mcp = FastMCP("My MCP Server")
 
 @mcp.tool
-def moveStraight(name: str) -> str:
+def moveToA(name: str) -> str:
     """
-    调用机器人直行接口
+    调用机器人去A点的接口
     """
     try:
-        response = requests.post("http://10.3.51.233:8080/go-straight")
+        response = requests.post("http://myir:8080/go-to-a")
         # if response.status_code == 200:
         return f"指令 '{name}' 执行成功，机器人已直行。"
         # else:
@@ -17,6 +17,33 @@ def moveStraight(name: str) -> str:
     except requests.RequestException as e:
         return f"请求错误: {e}"
 
+@mcp.tool
+def moveToB(name: str) -> str:
+    """
+    调用机器人去B点的接口
+    """
+    try:
+        response = requests.post("http://myir:8080/go-to-b")
+        # if response.status_code == 200:
+        return f"指令 '{name}' 执行成功，机器人已返回。"
+        # else:
+        #     return f"调用失败，状态码: {response.status_code}"
+    except requests.RequestException as e:
+        return f"请求错误: {e}"
+
+@mcp.tool
+def moveBack(name: str) -> str:
+    """
+    调用机器人返回的接口
+    """
+    try:
+        response = requests.post("http://myir:8080/go-back")
+        # if response.status_code == 200:
+        return f"指令 '{name}' 执行成功，机器人已返回。"
+        # else:
+        #     return f"调用失败，状态码: {response.status_code}"
+    except requests.RequestException as e:
+        return f"请求错误: {e}"
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="127.0.0.1", port=9000)
+    mcp.run(transport="sse", host="127.0.0.1", port=9000)
